083dc98d47775caca065a842bae7d1c0d403cbc9
chizukicn <chizukicn@outlook.com>
2024-12-19 22:54:46 +0800

    chore: update

--- playground/main.tsx
+++ playground/main.tsx
@@ -5,9 +5,9 @@ createApp({
   setup() {
     return () => (
       <div class="bg-secondary duration-300 flex h-full w-full items-center justify-center">
-          <div class="bg-white text-pink-3 text-4xl animate__zoomInUp animate__animated animate__delay-0.5s" >
-            UnPreset Animate.CSS
-          </div>
+        <div class="bg-white text-pink-3 text-4xl animate__zoomInUp animate__animated animate__delay-0.5s">
+          UnPreset Animate.CSS
+        </div>
       </div>
     )
   },
--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-
 import jsx from '@vitejs/plugin-vue-jsx'
 
 import uno from 'unocss/vite'
+
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
